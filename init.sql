1a037ddc7be81c0e01e9043cea4a9f6bd2266b37
Loisetal <lolo.talluau@gmail.com>
2025-09-22 10:45:39 +0200

    insert in BDD

--- init.sql
+++ init.sql
@@ -27,3 +27,26 @@ CREATE TABLE loans (
     return_date TIMESTAMP,
     status BOOLEAN DEFAULT TRUE
 );
+
+
+INSERT INTO users (name, email, phone, password)
+VALUES 
+('Alice Dupont', 'alice.dupont@example.com', '0601020304', 'password1'),
+('Bob Martin', 'bob.martin@example.com', '0605060708', 'password2'),
+('Charlie Durand', 'charlie.durand@example.com', NULL, 'password3'),
+('Diane Leroy', 'diane.leroy@example.com', '0611121314', 'password4');
+
+INSERT INTO books (title, author, genre, publication_date)
+VALUES 
+('1984', 'George Orwell', 'Dystopie', '1949-06-08'),
+('Le Petit Prince', 'Antoine de Saint-Exupéry', 'Conte', '1943-04-06'),
+('Harry Potter à l''école des sorciers', 'J.K. Rowling', 'Fantasy', '1997-06-26'),
+('Les Misérables', 'Victor Hugo', 'Classique', '1862-01-01'),
+('L''Étranger', 'Albert Camus', 'Philosophie', '1942-05-19');
+
+INSERT INTO loans (user_id, book_id, loan_date, return_date, status)
+VALUES
+(1, 1, '2025-09-01 10:00:00', '2025-09-10 15:00:00', FALSE),  -- Alice a rendu 1984
+(2, 2, '2025-09-15 09:30:00', NULL, TRUE),                     -- Bob a emprunté Le Petit Prince
+(3, 3, '2025-09-18 14:00:00', NULL, TRUE),                     -- Charlie a emprunté Harry Potter
+(4, 4, '2025-09-10 11:45:00', '2025-09-20 16:30:00', FALSE);  -- Diane a rendu Les Misérables
